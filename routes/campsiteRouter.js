4be0d07ad546f183cd71f2fa4539ffa8b9991be4
Ashley Blemler <ashleyblemler@gmail.com>
2022-02-04 19:01:24 -0800

    Passport JWT

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const Campsite = require("../models/campsite");
+const authenticate = require('../authenticate');
+
 
 const campsiteRouter = express.Router();
 
 campsiteRouter
-  .route("/:campsiteId/comments")
+  .route('/')
   .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
@@ -20,7 +22,7 @@ campsiteRouter
       })
       .catch((err) => next(err));
   })
-  .post((req, res, next) => {
+  .post(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
         if (campsite) {
@@ -41,13 +43,13 @@ campsiteRouter
       })
       .catch((err) => next(err));
   })
-  .put((req, res) => {
+  .put(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end(
       `PUT operation not supported on /campsites/${req.params.campsiteId}/comments`
     );
   })
-  .delete((req, res, next) => {
+  .delete(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
         if (campsite) {
@@ -92,13 +94,13 @@ campsiteRouter
       })
       .catch((err) => next(err));
   })
-  .post((req, res) => {
+  .post((authenticate.verifyUser, req, res) => {
     res.statusCode = 403;
     res.end(
       `POST operation not supported on /campsites/${req.params.campsiteId}/comments/${req.params.commentId}`
     );
   })
-  .put((req, res, next) => {
+  .put((authenticate.verifyUser, req, res, next) => {
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
@@ -128,7 +130,7 @@ campsiteRouter
       })
       .catch((err) => next(err));
   })
-  .delete((req, res, next) => {
+  .delete((authenticate.verifyUser, req, res, next) => {
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
