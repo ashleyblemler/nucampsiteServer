e075e7c35edbc8e1b944921dc245714cce9cf4a8
Ashley Blemler <ashleyblemler@gmail.com>
2022-02-11 17:57:22 -0800

    Week 4 Workshop

---
+++ models/favorite.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema; //making shorthand to the mongoose.schema function, so we can refer to it as schema instead of mongoose.schema
+
+const favoriteSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    campsites: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Campsite",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Favorite = mongoose.model("Favorite", favoriteSchema);
+
+module.exports = Favorite;
